5f9bdefe5641d8a3dbc7974f7f2a8a76d10f94c2
TheIdleGrinder <vincenthamann88@gmail.com>
2022-12-18 00:57:58 +0100

    * Added new tutorial states

--- TheIdleScrolls_Core/Systems/TutorialSystem.cs
+++ TheIdleScrolls_Core/Systems/TutorialSystem.cs
@@ -9,10 +9,16 @@ using TheIdleScrolls_Core.Items;
 
 namespace TheIdleScrolls_Core.Systems
 {
-    public enum TutorialProgress { Start, Inventory }
+    public enum TutorialProgress { Start, Inventory, MobAttacks, Armor, Abilities, Travel }
 
     public class TutorialSystem : AbstractSystem
     {
+        static int LvlInventory = 2;
+        static int LvlMobAttacks = 5;
+        static int LvlArmor = 10;
+        static int LvlAbilities = 15;
+        static int LvlTravel = 20;
+
         TutorialProgress m_progress = TutorialProgress.Start;
 
         Entity? m_player = null;
@@ -26,6 +32,8 @@ namespace TheIdleScrolls_Core.Systems
             if (m_player == null)
                 return;
 
+            int lvl = m_player.GetComponent<LevelComponent>()?.Level ?? 0;
+
             // Determine previous progress
             if (m_firstUpdate)
             {
@@ -33,38 +41,79 @@ namespace TheIdleScrolls_Core.Systems
 
                 if (m_player.HasComponent<InventoryComponent>())
                     m_progress = TutorialProgress.Inventory;
+                if (m_progress == TutorialProgress.Inventory && lvl >= LvlMobAttacks)
+                    m_progress = TutorialProgress.MobAttacks;
+                if (m_progress == TutorialProgress.MobAttacks && lvl >= LvlArmor)
+                    m_progress = TutorialProgress.Armor;
+                if (m_progress == TutorialProgress.Armor && lvl >= LvlAbilities)
+                    m_progress = TutorialProgress.Abilities;
+                if (m_progress == TutorialProgress.Abilities && lvl >= LvlTravel)
+                    m_progress = TutorialProgress.Travel;
             }
 
             // Evaluate conditions for current tutorial stage
-            if (m_progress == TutorialProgress.Start)
+            if (m_progress == TutorialProgress.Start && lvl >= LvlInventory)
             {
-                int lvl = m_player.GetComponent<LevelComponent>()?.Level ?? 0;
-                if (lvl >= 2)
+                InventoryComponent invComp = new();
+                List<string> weapons = new() { "SBL0", "LBL0", "AXE0", "BLN0", "POL0", "LAR0", "HAR0" };
+                ItemFactory factory = new();
+
+                m_player.AddComponent(invComp);
+                m_player.AddComponent(new EquipmentComponent());
+                string itemString = "";
+                foreach (var weaponCode in weapons)
                 {
-                    InventoryComponent invComp = new();
-                    List<string> weapons = new() { "SBL0", "LBL0", "AXE0", "BLN0", "POL0", "LAR0", "HAR0" };
-                    ItemFactory factory = new();
-
-                    m_player.AddComponent(invComp);
-                    m_player.AddComponent(new EquipmentComponent());
-
-                    foreach (var weaponCode in weapons)
+                    Entity? weapon = factory.ExpandCode(weaponCode);
+                    if (weapon != null)
                     {
-                        Entity? weapon = factory.ExpandCode(weaponCode);
-                        if (weapon != null)
-                        {
-                            coordinator.AddEntity(weapon);
-                            coordinator.PostMessage(this, new ItemReceivedMessage(m_player, weapon));
-                        }
+                        itemString += $"\n  - Received '{weapon.GetName()}'";
+                        coordinator.AddEntity(weapon);
+                        coordinator.PostMessage(this, new ItemReceivedMessage(m_player, weapon));
                     }
-                    
-                    m_progress = TutorialProgress.Inventory;
-                    coordinator.PostMessage(this,
-                        new TutorialMessage(m_progress, "Level Up!", "You have unlocked the inventory. Time to gear up!"));
                 }
-            } else if (false)
+                    
+                m_progress = TutorialProgress.Inventory;
+                coordinator.PostMessage(this,
+                    new TutorialMessage(m_progress, "Level Up!", 
+                    $"You have unlocked the inventory. Time to gear up!\n  - Unlocked inventory{itemString}"));
+            }
+            else if (m_progress == TutorialProgress.Inventory && lvl >= LvlMobAttacks)
             {
-                // ...
+                m_progress = TutorialProgress.MobAttacks;
+                coordinator.PostMessage(this,
+                    new TutorialMessage(m_progress, "Training is over",
+                    $"From this point on, mobs are going to fight back. Watch the countdown near the mob. If time runs out, you lose the fight."));
+            
+            }
+            else if (m_progress == TutorialProgress.MobAttacks && lvl >= LvlArmor)
+            {
+                List<string> items = new() { "LAR0", "HAR0" };
+                ItemFactory factory = new();
+
+                string itemString = "";
+                foreach (var itemCode in items)
+                {
+                    Entity? item = factory.ExpandCode(itemCode);
+                    if (item != null)
+                    {
+                        itemString += $"\n  - Received '{item.GetName()}'";
+                        coordinator.AddEntity(item);
+                        coordinator.PostMessage(this, new ItemReceivedMessage(m_player, item));
+                    }
+                }
+
+                m_progress = TutorialProgress.Armor;
+                coordinator.PostMessage(this,
+                    new TutorialMessage(m_progress, "It's dangerous to go alone", 
+                    $"Those mobs are getting nasty. Use armor to slow down the countdown during fights.{itemString}"));
+            }
+            else if (m_progress == TutorialProgress.Armor && lvl >= LvlAbilities)
+            {
+                m_progress = TutorialProgress.Abilities;
+                coordinator.PostMessage(this,
+                    new TutorialMessage(m_progress, "Live and learn",
+                    $""));
+
             }
         }
     }
