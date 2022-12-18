9951c885b6f49154aad061a0864062de5e93abc8
TheIdleGrinder <vincenthamann88@gmail.com>
2022-12-18 23:12:29 +0100

    * TutorialSystem now uses PlayerProgressComponent

---
+++ TheIdleScrolls_Core/Components/PlayerProgressComponent.cs
@@ -0,0 +1,21 @@
+﻿using MiniECS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheIdleScrolls_Core.Components
+{
+    public enum TutorialProgress { Start, Inventory, MobAttacks, Armor, Abilities, Travel }
+
+    internal class PlayerProgressComponent : IComponent
+    {
+        public ProgressData Data { get; set; } = new();
+    }
+
+    public class ProgressData
+    {
+        public TutorialProgress Progress { get; set; } = TutorialProgress.Start;
+    }
+}
--- TheIdleScrolls_Core/Systems/TutorialSystem.cs
+++ TheIdleScrolls_Core/Systems/TutorialSystem.cs
@@ -9,8 +9,6 @@ using TheIdleScrolls_Core.Items;
 
 namespace TheIdleScrolls_Core.Systems
 {
-    public enum TutorialProgress { Start, Inventory, MobAttacks, Armor, Abilities, Travel }
-
     public class TutorialSystem : AbstractSystem
     {
         static int LvlInventory = 2;
@@ -18,8 +16,6 @@ namespace TheIdleScrolls_Core.Systems
         static int LvlArmor = 10;
         static int LvlAbilities = 15;
         static int LvlTravel = 20;
-
-        TutorialProgress m_progress = TutorialProgress.Start;
 
         Entity? m_player = null;
 
@@ -29,7 +25,9 @@ namespace TheIdleScrolls_Core.Systems
         {
             if (m_player == null)
                 m_player = coordinator.GetEntities<PlayerComponent>().FirstOrDefault();
-            if (m_player == null)
+            var progComp = m_player?.GetComponent<PlayerProgressComponent>();
+            
+            if (m_player == null || progComp == null)
                 return;
 
             int lvl = m_player.GetComponent<LevelComponent>()?.Level ?? 0;
@@ -39,20 +37,26 @@ namespace TheIdleScrolls_Core.Systems
             {
                 m_firstUpdate = false;
 
+                var progress = TutorialProgress.Start;
                 if (m_player.HasComponent<InventoryComponent>())
-                    m_progress = TutorialProgress.Inventory;
-                if (m_progress == TutorialProgress.Inventory && lvl >= LvlMobAttacks)
-                    m_progress = TutorialProgress.MobAttacks;
-                if (m_progress == TutorialProgress.MobAttacks && lvl >= LvlArmor)
-                    m_progress = TutorialProgress.Armor;
-                if (m_progress == TutorialProgress.Armor && lvl >= LvlAbilities)
-                    m_progress = TutorialProgress.Abilities;
-                if (m_progress == TutorialProgress.Abilities && lvl >= LvlTravel)
-                    m_progress = TutorialProgress.Travel;
+                    progress = TutorialProgress.Inventory;
+                if (progress == TutorialProgress.Inventory && lvl >= LvlMobAttacks)
+                    progress = TutorialProgress.MobAttacks;
+                if (progress == TutorialProgress.MobAttacks && lvl >= LvlArmor)
+                    progress = TutorialProgress.Armor;
+                if (progress == TutorialProgress.Armor && lvl >= LvlAbilities)
+                    progress = TutorialProgress.Abilities;
+                if (progress == TutorialProgress.Abilities && lvl >= LvlTravel)
+                    progress = TutorialProgress.Travel;
+
+                if (progress > progComp.Data.Progress)
+                {
+                    progComp.Data.Progress = progress;
+                }
             }
 
             // Evaluate conditions for current tutorial stage
-            if (m_progress == TutorialProgress.Start && lvl >= LvlInventory)
+            if (progComp.Data.Progress == TutorialProgress.Start && lvl >= LvlInventory)
             {
                 InventoryComponent invComp = new();
                 List<string> weapons = new() { "SBL0", "LBL0", "AXE0", "BLN0", "POL0", "LAR0", "HAR0" };
@@ -71,21 +75,21 @@ namespace TheIdleScrolls_Core.Systems
                         coordinator.PostMessage(this, new ItemReceivedMessage(m_player, weapon));
                     }
                 }
-                    
-                m_progress = TutorialProgress.Inventory;
+
+                progComp.Data.Progress = TutorialProgress.Inventory;
                 coordinator.PostMessage(this,
-                    new TutorialMessage(m_progress, "Level Up!", 
+                    new TutorialMessage(progComp.Data.Progress, "Level Up!", 
                     $"You have unlocked the inventory. Time to gear up!\n  - Unlocked inventory{itemString}"));
             }
-            else if (m_progress == TutorialProgress.Inventory && lvl >= LvlMobAttacks)
+            else if (progComp.Data.Progress == TutorialProgress.Inventory && lvl >= LvlMobAttacks)
             {
-                m_progress = TutorialProgress.MobAttacks;
+                progComp.Data.Progress = TutorialProgress.MobAttacks;
                 coordinator.PostMessage(this,
-                    new TutorialMessage(m_progress, "Training is over",
+                    new TutorialMessage(progComp.Data.Progress, "Training is over",
                     $"From this point on, mobs are going to fight back. Watch the countdown near the mob. If time runs out, you lose the fight."));
             
             }
-            else if (m_progress == TutorialProgress.MobAttacks && lvl >= LvlArmor)
+            else if (progComp.Data.Progress == TutorialProgress.MobAttacks && lvl >= LvlArmor)
             {
                 List<string> items = new() { "LAR0", "HAR0" };
                 ItemFactory factory = new();
@@ -102,17 +106,25 @@ namespace TheIdleScrolls_Core.Systems
                     }
                 }
 
-                m_progress = TutorialProgress.Armor;
+                progComp.Data.Progress = TutorialProgress.Armor;
                 coordinator.PostMessage(this,
-                    new TutorialMessage(m_progress, "It's dangerous to go alone", 
+                    new TutorialMessage(progComp.Data.Progress, "It's dangerous to go alone", 
                     $"Those mobs are getting nasty. Use armor to slow down the countdown during fights.{itemString}"));
             }
-            else if (m_progress == TutorialProgress.Armor && lvl >= LvlAbilities)
+            else if (progComp.Data.Progress == TutorialProgress.Armor && lvl >= LvlAbilities)
             {
-                m_progress = TutorialProgress.Abilities;
+                progComp.Data.Progress = TutorialProgress.Abilities;
                 coordinator.PostMessage(this,
-                    new TutorialMessage(m_progress, "Live and learn",
-                    $""));
+                    new TutorialMessage(progComp.Data.Progress, "Live and learn",
+                    $"\n  - Unlocked abilities"));
+
+            }
+            else if (progComp.Data.Progress == TutorialProgress.Abilities && lvl >= LvlTravel)
+            {
+                progComp.Data.Progress = TutorialProgress.Travel;
+                coordinator.PostMessage(this,
+                    new TutorialMessage(progComp.Data.Progress, "Freedom of movement",
+                    $"\n  - Unlocked abilities"));
 
             }
         }
