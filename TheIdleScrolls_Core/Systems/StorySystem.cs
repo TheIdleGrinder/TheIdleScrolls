3989ba3f6237b086708b2dc248e28f7850b823b0
TheIdleGrinder <vincenthamann88@gmail.com>
2023-03-30 00:57:24 +0200

    * HP and time limit are now rescaled during the final fight if the players equipment changes

--- TheIdleScrolls_Core/Systems/StorySystem.cs
+++ TheIdleScrolls_Core/Systems/StorySystem.cs
@@ -18,8 +18,15 @@ namespace TheIdleScrolls_Core.Systems
         public override void Update(World world, Coordinator coordinator, double dt)
         {
             m_player ??= coordinator.GetEntities<PlayerComponent>().FirstOrDefault();
+            
+            HandleFinalFight(world, coordinator);
+        }
+
+        void HandleFinalFight(World world, Coordinator coordinator)
+        {
             if (m_player == null)
                 return;
+
             var storyComp = m_player.GetComponent<StoryProgressComponent>();
             if (storyComp == null)
                 return;
@@ -29,7 +36,7 @@ namespace TheIdleScrolls_Core.Systems
                 // Reset game speed
                 world.GameOver = false;
                 world.SpeedMultiplier = 1.0;
-                storyComp.FinalFight.State = FinalFight.Status.NotStarted;                
+                storyComp.FinalFight.State = FinalFight.Status.NotStarted;
             }
 
             if (storyComp.FinalFight.State == FinalFight.Status.NotStarted)
@@ -39,37 +46,48 @@ namespace TheIdleScrolls_Core.Systems
                     && world.RemainingEnemies == 1
                     && coordinator.GetEntities<MobComponent>().FirstOrDefault() != null)
                 {
-                    var mob = coordinator.GetEntities<MobComponent>().FirstOrDefault();
-                    if (mob == null)
-                        throw new Exception("Final mob was not found");
-
                     storyComp.FinalFight.State = FinalFight.Status.Slowing;
                     storyComp.FinalFight.StartTime = DateTime.Now;
 
                     // Transform mob into final boss
+                    var mob = coordinator.GetEntities<MobComponent>().FirstOrDefault();
+                    if (mob == null)
+                        throw new Exception("Final mob was not found");
                     mob.AddComponent(new NameComponent(Properties.LocalizedStrings.BOSS_FINAL_DEMON));
                     mob.AddComponent(new MobDamageComponent(1.0));
+                    world.TimeLimit.Reset();
 
-                    // Set HP high enought to prevent deafeating the boss
-                    var attackComp = m_player.GetComponent<AttackComponent>();
-                    if (attackComp != null)
-                    {
-                        double dps = attackComp.RawDamage / attackComp.Cooldown.Duration;
-                        mob.AddComponent(new LifePoolComponent((int)(dps * slopeDuration)));
-                    }
+                    // Set HP high enough to prevent deafeating the boss
+                    //var attackComp = m_player.GetComponent<AttackComponent>();
+                    //if (attackComp != null)
+                    //{
+                    //    double dps = attackComp.RawDamage / attackComp.Cooldown.Duration;
+                    //    mob.AddComponent(new LifePoolComponent((int)(dps * slopeDuration)));
+                    //}
 
-                    var defenseComp = m_player.GetComponent<DefenseComponent>();
-                    if (defenseComp != null)
-                    {
-                        double multi = 1.0 + (defenseComp.Armor / 100.0); // CornerCut: Have central function for bonus
-                        world.TimeLimit.Reset(1.0 * slopeDuration / multi);
-                    }
+                    //var defenseComp = m_player.GetComponent<DefenseComponent>();
+                    //if (defenseComp != null)
+                    //{
+                    //    double multi = 1.0 + (defenseComp.Armor / 100.0); // CornerCut: Have central function for bonus
+                    //    world.TimeLimit.Reset(1.0 * slopeDuration / multi);
+                    //}
+                    ScaleMobHpAndTimeLimit(m_player, mob, world);
                 }
-            } 
+            }
             else if (storyComp.FinalFight.State == FinalFight.Status.Slowing)
             {
                 double duration = (DateTime.Now - storyComp.FinalFight.StartTime).Seconds;
                 world.SpeedMultiplier = 1.0 - Math.Min(Math.Pow(duration, 0.25) / Math.Pow(slopeDuration, 0.25), 1.0);
+
+                // Rescale HP and time on gear change
+                if (coordinator.MessageTypeIsOnBoard<ItemMovedMessage>())
+                {
+                    var mob = coordinator.GetEntities<MobComponent>().FirstOrDefault();
+                    if (mob == null)
+                        throw new Exception("Final mob was not found");
+                    ScaleMobHpAndTimeLimit(m_player, mob, world);
+                }
+
                 if (duration >= slopeDuration)
                 {
                     storyComp.FinalFight.State = FinalFight.Status.Pause;
@@ -90,11 +108,36 @@ namespace TheIdleScrolls_Core.Systems
                 bool first = !m_player.GetComponent<PlayerProgressComponent>()?.Data.DungeonTimes.ContainsKey(world.DungeonId) ?? true;
                 coordinator.PostMessage(this, new ManualSaveRequest());
                 coordinator.PostMessage(this, new DungeonCompletedMessage(world.DungeonId.Localize(), first));
-                coordinator.PostMessage(this, new TutorialMessage(TutorialStep.Finished, 
-                    Properties.LocalizedStrings.STORY_END_TITLE, 
+                coordinator.PostMessage(this, new TutorialMessage(TutorialStep.Finished,
+                    Properties.LocalizedStrings.STORY_END_TITLE,
                     String.Format(Properties.LocalizedStrings.STORY_END_TEXT, playtime)));
                 storyComp.FinalFight.State = FinalFight.Status.Finished;
                 world.GameOver = true;
+            }
+        }
+
+        static void ScaleMobHpAndTimeLimit(Entity player, Entity mob, World world)
+        {
+            double baseMultiplier = 0.35;
+            // Set HP high enough to prevent deafeating the boss
+            var attackComp = player.GetComponent<AttackComponent>();
+            if (attackComp != null)
+            {
+                var hpComp = mob.GetComponent<LifePoolComponent>() ?? new LifePoolComponent();
+                double remaining = 1.0 * hpComp.Current / hpComp.Maximum;
+                double dps = attackComp.RawDamage / attackComp.Cooldown.Duration;
+                hpComp.Maximum = (int)(baseMultiplier * dps * slopeDuration);
+                hpComp.Current = (int)(remaining * hpComp.Maximum);
+                mob.AddComponent(hpComp);
+            }
+
+            // Set time limit to prevent the player from losing
+            var defenseComp = player.GetComponent<DefenseComponent>();
+            if (defenseComp != null)
+            {
+                double multi = 1.0 + (defenseComp.Armor / 100.0); // CornerCut: Have central function for bonus
+                double targetDuration = slopeDuration / multi;
+                world.TimeLimit.ChangeDuration(baseMultiplier * targetDuration);
             }
         }
     }
