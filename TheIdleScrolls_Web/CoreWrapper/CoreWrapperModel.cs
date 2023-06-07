a49f698fcc9a3c70ceb88d500615f61c9955d6da
TheIdleGrinder <119699620+TheIdleGrinder@users.noreply.github.com>
2023-06-07 23:50:01 +0200

    * Added project and folder structure for web app

---
+++ TheIdleScrolls_Web/CoreWrapper/CoreWrapperModel.cs
@@ -0,0 +1,31 @@
+﻿using MiniECS;
+using TheIdleScrolls_Core;
+using TheIdleScrolls_Core.DataAccess;
+using TheIdleScrolls_Core.Items;
+using TheIdleScrolls_Core.Storage;
+using TheIdleScrolls_Storage;
+
+namespace TheIdleScrolls_Web.CoreWrapper
+{
+    public class CoreWrapperModel : IApplicationModel
+    {
+        DataAccessHandler dataHandler;
+        GameRunner gameRunner;
+
+        public CoreWrapperModel()
+        {
+            dataHandler = new DataAccessHandler(new EntityJsonConverter(new ItemFactory()), new BasicFileStorageHandler());
+            gameRunner = new GameRunner(dataHandler);
+        }
+
+        public HashSet<IMessage.PriorityLevel> GetRelevantMessagePriorties()
+        {
+            return new() { IMessage.PriorityLevel.High, IMessage.PriorityLevel.VeryHigh };
+        }
+
+        public List<string> GetSavedCharacters()
+        {
+            return dataHandler.ListStoredEntities();
+        }
+    }
+}
