8e41c2bc81033a8a6cfe1928c0769dcaf8241ba4
TheIdleGrinder <119699620+TheIdleGrinder@users.noreply.github.com>
2023-06-29 22:42:18 +0200

    * Items can now be unequipped in equipment component

---
+++ TheIdleScrolls_Web/Components/CssUtilities.cs
@@ -0,0 +1,27 @@
+﻿using TheIdleScrolls_Core;
+
+namespace TheIdleScrolls_Web.Components
+{
+    public class CssUtilities
+    {
+        public static string GetClassesForItem(ItemRepresentation? item)
+        {
+            string classes = "";
+            if (item == null || item.Rarity < 0)
+            {
+                classes += " rarity-none";
+            }
+            else
+            {
+                classes += $" rarity-{item.Rarity}";
+                if (item.Rarity > 0)
+                    classes += " rare-item";
+            }
+
+            if (item?.Crafted ?? false)
+                classes += " crafted";
+
+            return classes;
+        }
+    }
+}
