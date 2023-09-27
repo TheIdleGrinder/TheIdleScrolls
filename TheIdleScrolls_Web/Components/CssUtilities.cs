14d3e8d3817e14c649cab1ce5e7cd4ef88e54a2d
TheIdleGrinder <119699620+TheIdleGrinder@users.noreply.github.com>
2023-09-27 23:32:35 +0200

    * Added shorter representation of long numbers

--- TheIdleScrolls_Web/Components/CssUtilities.cs
+++ TheIdleScrolls_Web/Components/CssUtilities.cs
@@ -38,4 +38,25 @@ namespace TheIdleScrolls_Web.Components
             return classes;
         }
     }
+
+    public static class ExtensionMethods
+    {
+        public static string ToBigIntString(this int value)
+        {
+            if (value < 1e5)
+                return value.ToString();
+
+            int exponent = (int)Math.Log10(value);
+            string suffix = new string[] { "", "K", "M", "B" } [exponent / 3];
+            int remainder = exponent % 3;
+            int baseExponent = exponent - remainder;
+            double fraction = value / Math.Pow(10, baseExponent);
+            return remainder switch
+            {
+                0 => $"{fraction:0.##}",
+                1 => $"{fraction:0.#}",
+                _ => $"{fraction:0}"
+            } + suffix;
+        }
+    }
 }
