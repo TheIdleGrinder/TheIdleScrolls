fd031204617b0143a544b38830d822392814580e
TheIdleGrinder <119699620+TheIdleGrinder@users.noreply.github.com>
2023-10-15 14:28:13 +0200

    * Added methods to IZoneGenerator

--- TheIdleScrolls_Core/GameWorld/IZoneGenerator.cs
+++ TheIdleScrolls_Core/GameWorld/IZoneGenerator.cs
@@ -8,6 +8,12 @@ namespace TheIdleScrolls_Core.GameWorld
 {
     public interface IZoneGenerator
     {
+        public ZoneDescription? GetZone(Location location);
 
+        public List<DungeonDescription> GetDungeons();
+
+        public List<DungeonDescription> GetDungeonsAtLocation(Location location);
+
+        public ZoneDescription? GetDungeonZone(string dungeonId, int floor);
     }
 }
