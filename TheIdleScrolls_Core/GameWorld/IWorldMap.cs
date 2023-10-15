930175f1e9ab7fc8df2f81916cf9cd9b2c2b9e7c
TheIdleGrinder <119699620+TheIdleGrinder@users.noreply.github.com>
2023-10-15 14:38:31 +0200

    * Added IWorldMap

---
+++ TheIdleScrolls_Core/GameWorld/IWorldMap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheIdleScrolls_Core.GameWorld
+{
+    public interface IWorldMap : IZoneGenerator
+    {
+        public ZoneDescription? GetNextLocation(Location location);
+
+        public ZoneDescription? GetPreviousLocation(Location location);
+    }
+}
