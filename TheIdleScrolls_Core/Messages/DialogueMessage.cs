3a473f8ed3405cb3dbbb36c80a78f07a8a8732ba
TheIdleGrinder <119699620+TheIdleGrinder@users.noreply.github.com>
2023-11-11 01:28:48 +0100

    * Added two new messages for interactive dialogues

---
+++ TheIdleScrolls_Core/Messages/DialogueMessage.cs
@@ -0,0 +1,37 @@
+﻿using MiniECS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheIdleScrolls_Core.Messages
+{
+    public class DialogueMessage : IMessage
+    {
+        public string ResponseId { get; set; }
+        public string Speaker { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public List<string> ResponseOptions { get; set; }
+
+        public DialogueMessage(string responseId, string speaker, string title, string message, List<string> responseOptions)
+        {
+            ResponseId = responseId;
+            Speaker = speaker;
+            Title = title;
+            Message = message;
+            ResponseOptions = responseOptions;
+        }
+
+        string IMessage.BuildMessage()
+        {
+            return $"[{Title}@{ResponseId}]\n{Speaker}: {Message}" + ResponseOptions.Aggregate((agg, r) => $"{agg}\n- {r}");
+        }
+
+        IMessage.PriorityLevel IMessage.GetPriority()
+        {
+            return IMessage.PriorityLevel.Debug;
+        }
+    }
+}
---
+++ TheIdleScrolls_Core/Messages/DialogueResponseMessage.cs
@@ -0,0 +1,31 @@
+﻿using MiniECS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheIdleScrolls_Core.Messages
+{
+    public class DialogueResponseMessage : IMessage
+    {
+        public string ResponseId { get; set; }
+        public string Response { get; set; }
+
+        public DialogueResponseMessage(string responseId, string response)
+        {
+            ResponseId = responseId;
+            Response = response;
+        }
+
+        string IMessage.BuildMessage()
+        {
+            return $"Response @{ResponseId}: {Response}";
+        }
+
+        IMessage.PriorityLevel IMessage.GetPriority()
+        {
+            return IMessage.PriorityLevel.Debug;
+        }
+    }
+}
