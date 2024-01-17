8fc15f7a1879150a080a6fc00a321c004f52f6b0
TheIdleGrinder <119699620+TheIdleGrinder@users.noreply.github.com>
2024-01-17 22:31:18 +0100

    * Added TagsComponent for storing tags associated with an entity

---
+++ TheIdleScrolls_Core/Components/TagsComponent.cs
@@ -0,0 +1,39 @@
+﻿using MiniECS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheIdleScrolls_Core.Components
+{
+    public class TagsComponent : IComponent
+    {
+        private HashSet<string> m_tags = new();
+
+        public TagsComponent() { }
+
+        public TagsComponent(IEnumerable<string> tags)
+        {
+            AddTags(tags);
+        }
+
+        public bool HasTag(string tag) => m_tags.Contains(tag);
+
+        public bool AddTag(string tag) => m_tags.Add(tag);
+
+        public void AddTags(IEnumerable<string> tags)
+        {
+            foreach (string tag in tags)
+            {
+                m_tags.Add(tag);
+            }
+        }
+
+        public bool RemoveTag(string tag) => m_tags.Remove(tag);
+
+        public List<string> ListTags() => m_tags.OrderBy(x => x).ToList();
+
+
+    }
+}
