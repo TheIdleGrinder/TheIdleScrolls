f67a7d97d2c12b76159dc2e84c88d9a5c12f73b1
TheIdleGrinder <119699620+TheIdleGrinder@users.noreply.github.com>
2024-01-26 00:53:12 +0100

    * Added Perk class

---
+++ TheIdleScrolls_Core/Modifiers/Perk.cs
@@ -0,0 +1,48 @@
+﻿using MiniECS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheIdleScrolls_Core.GameWorld;
+
+namespace TheIdleScrolls_Core.Modifiers
+{
+    public enum UpdateTrigger
+    {
+        BattleStarted,
+        BattleFinished,
+        AttackPerformed,
+        AbilityIncreased,
+        EquipmentChanged
+    }
+
+    public class Perk
+    {
+        public string Id { get; set; }
+        public string Name { get; set; } = "??";
+        public string Description { get; set; } = "??";
+        public List<Modifier> Modifiers { get; private set; } = new();
+        public HashSet<UpdateTrigger> UpdateTriggers { get; private set; } = new();
+        public Func<Entity, World, Coordinator, List<Modifier>> ModifiersFunc { get; private set; } 
+            = (Entity e, World w, Coordinator c) => { return new(); };
+
+        public Perk(string id, string name, string description, 
+            List<Modifier> modifiers, 
+            HashSet<UpdateTrigger> updateTriggers, 
+            Func<Entity, World, Coordinator, List<Modifier>> modifiersFunc)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Modifiers = modifiers;
+            UpdateTriggers = updateTriggers;
+            ModifiersFunc = modifiersFunc;
+        }
+
+        public void UpdateModifiers(Entity owner, World world, Coordinator coordinator)
+        {
+            Modifiers = ModifiersFunc(owner, world, coordinator);
+        }
+    }
+}
