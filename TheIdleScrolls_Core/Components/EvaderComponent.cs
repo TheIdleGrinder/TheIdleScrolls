cdbee0a54e8dbde876579bd0067a5055237eeb18
TheIdleGrinder <119699620+TheIdleGrinder@users.noreply.github.com>
2024-11-02 20:14:51 +0100

    Added classes for EvaderComponent and EvasionSystem

---
+++ TheIdleScrolls_Core/Components/EvaderComponent.cs
@@ -0,0 +1,23 @@
+﻿using MiniECS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheIdleScrolls_Core.Utility;
+
+namespace TheIdleScrolls_Core.Components
+{
+    public class EvaderComponent : IComponent
+    {
+        public bool Active { get; set; } = false;
+        public double ChargeDuration { get; set; } = 0.0;
+        public double EvasionDuration { get; set; } = 0.0;
+        public Cooldown Duration { get; set; } = new Cooldown(1.0);
+
+        public EvaderComponent()
+        {
+            Duration.SingleShot = true;
+        }
+    }
+}
---
+++ TheIdleScrolls_Core/Systems/EvasionSystem.cs
@@ -0,0 +1,73 @@
+﻿using MiniECS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheIdleScrolls_Core.Components;
+using TheIdleScrolls_Core.GameWorld;
+
+namespace TheIdleScrolls_Core.Systems
+{
+    public class EvasionSystem : AbstractSystem
+    {
+        public override void Update(World world, Coordinator coordinator, double dt)
+        {
+            foreach (var entity in coordinator.GetEntities<DefenseComponent>())
+            {
+                var defenseComponent = entity.GetComponent<DefenseComponent>()!;
+                var evaderComponent = entity.GetComponent<EvaderComponent>();
+
+                if (defenseComponent.Evasion <= 0)
+                {
+                    if (evaderComponent != null)
+                    {
+                        entity.RemoveComponent<EvaderComponent>();
+                    }
+                    continue;
+                }
+                else
+                {
+                    if (evaderComponent == null)
+                    {
+                        evaderComponent = new();
+                        entity.AddComponent(evaderComponent);
+                    }
+
+                    var battlerComp = entity.GetComponent<BattlerComponent>();
+                    if (battlerComp == null)
+                        continue;
+
+                    if (battlerComp.Battle.IsFinished)
+                    {
+                        ResetEvaderComponent(evaderComponent);
+                    }
+                    else if (battlerComp.Battle.State == Battle.BattleState.InProgress)
+                    {
+                        UpdateEvaderComponent(evaderComponent, dt);
+                    }
+                }
+            }
+        }
+
+        private static void UpdateEvaderComponent(EvaderComponent evaderComponent, double dt)
+        {
+            evaderComponent.Duration.Update(dt);
+            if (evaderComponent.Duration.HasFinished)
+            {
+                evaderComponent.Active = !evaderComponent.Active;
+                evaderComponent.Duration.Reset();
+                evaderComponent.Duration.ChangeDuration(evaderComponent.Active 
+                                                        ? evaderComponent.EvasionDuration 
+                                                        : evaderComponent.ChargeDuration);
+            }
+        }
+
+        private static void ResetEvaderComponent(EvaderComponent evaderComponent)
+        {
+            evaderComponent.Active = false;
+            evaderComponent.Duration.Reset();
+            evaderComponent.Duration.ChangeDuration(evaderComponent.ChargeDuration);
+        }
+    }
+}
