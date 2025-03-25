c53a8a97090c64109873646960a723b535b51f48
TheIdleGrinder <119699620+TheIdleGrinder@users.noreply.github.com>
2025-03-26 00:13:36 +0100

    Added changelog previewing changes in version 0.8.1 (#21)
    * Added changelog to app
    
    * Updated changelog
    
    * Added missing word

---
+++ TheIdleScrolls_Web/Definitions/Version.cs
@@ -0,0 +1,42 @@
+﻿namespace TheIdleScrolls_Web.Definitions
+{
+    public static class GameVersion
+    {
+        const uint Major = 0;
+        const uint Minor = 8;
+        const uint Patch = 0;
+
+        public static string GetCurrentVersionString()
+        {
+            return GetVersionString(Major, Minor, Patch);
+        }
+
+        public static string GetVersionString(uint major, uint minor, uint patch)
+        {
+            return $"v.{major}.{minor}.{patch}";
+        }
+
+        public static bool IsAtleast(uint major, uint minor, uint patch)
+        {
+            if (Major > major)
+            {
+                return true;
+            }
+            else if (Major == major)
+            {
+                if (Minor > minor)
+                {
+                    return true;
+                }
+                else if (Minor == minor)
+                {
+                    if (Patch >= patch)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
