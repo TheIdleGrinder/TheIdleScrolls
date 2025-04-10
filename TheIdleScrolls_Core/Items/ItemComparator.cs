52a0f18cbd8a4b895f7e015ca00ed620feca4d10
TheIdleGrinder <119699620+TheIdleGrinder@users.noreply.github.com>
2025-04-11 00:21:26 +0200

    Prototype of live comparison with worn items

---
+++ TheIdleScrolls_Core/Items/ItemComparator.cs
@@ -0,0 +1,82 @@
+﻿using MiniECS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheIdleScrolls_Core.Components;
+
+namespace TheIdleScrolls_Core.Items
+{
+    using ValueExtractor = Func<Entity, double>;
+    using CandidateComparator = Func<Entity, List<Entity>, List<RelativeValue>>;
+
+    public enum RelativeQuality
+    {
+        Better,
+        Equal,
+        Worse
+    }
+
+    public enum RelativeValue
+    {
+        Higher,
+        Equal,
+        Lower
+    }
+
+    public static class Extensions
+    {
+        public static RelativeQuality ToRelativeQuality(this RelativeValue value, bool higherIsBetter = true)
+        {
+            if (value == RelativeValue.Equal)
+            {
+                return RelativeQuality.Equal;
+            }
+            else
+            {
+                return (higherIsBetter && value == RelativeValue.Higher) || (!higherIsBetter && value == RelativeValue.Lower) 
+                        ? RelativeQuality.Better 
+                        : RelativeQuality.Worse;
+            }
+        }
+    }
+
+        public static class ItemComparator
+    {
+        private static ValueExtractor GetDamage => item => item.GetComponent<WeaponComponent>()?.Damage ?? 0.0;
+        private static ValueExtractor GetCooldown => item => item.GetComponent<WeaponComponent>()?.Cooldown ?? 0.0;
+        private static ValueExtractor GetDps => item => (item.GetComponent<WeaponComponent>()?.Damage ?? 0.0) /
+                                                        (item.GetComponent<WeaponComponent>()?.Cooldown ?? 1.0);
+        private static ValueExtractor GetArmor => item => item.GetComponent<ArmorComponent>()?.Armor ?? 0.0;
+        private static ValueExtractor GetEvasion => item => item.GetComponent<ArmorComponent>()?.Evasion ?? 0.0;
+        private static ValueExtractor GetEncumbrance => item => item.GetComponent<EquippableComponent>()?.Encumbrance ?? 0.0;
+
+        public static CandidateComparator CompareDamage      => (a, b) => Compare(a, b, GetDamage);
+        public static CandidateComparator CompareCooldown    => (a, b) => Compare(a, b, GetCooldown);
+        public static CandidateComparator CompareDps         => (a, b) => Compare(a, b, GetDps);
+        public static CandidateComparator CompareArmor       => (a, b) => Compare(a, b, GetArmor);
+        public static CandidateComparator CompareEvasion     => (a, b) => Compare(a, b, GetEvasion);
+        public static CandidateComparator CompareEncumbrance => (a, b) => Compare(a, b, GetEncumbrance);
+
+        private static RelativeValue Compare(Entity firstItem, Entity secondItem, ValueExtractor valueExtractor)
+        {
+            return CompareValues(valueExtractor(firstItem), valueExtractor(secondItem));
+        }
+
+        private static List<RelativeValue> Compare(Entity item, List<Entity> candidates, 
+                                                            ValueExtractor valueExtractor)
+        {
+            return candidates.Select(candidate => Compare(item, candidate, valueExtractor)).ToList();
+        }
+
+        private static RelativeValue CompareValues(double first, double second)
+        {
+            if (first == second)
+            {
+                return RelativeValue.Equal;
+            }
+            return first > second ? RelativeValue.Higher : RelativeValue.Lower;
+        }
+    }
+}
--- TheIdleScrolls_Core/Items/ItemEntityWrapper.cs
+++ TheIdleScrolls_Core/Items/ItemEntityWrapper.cs
@@ -14,20 +14,17 @@ namespace TheIdleScrolls_Core.Items
     {
         public uint Id => Item.Id;
         public string Name => Item.GetName();
-
         public string Description => GenerateDescription();
-
         public List<EquipmentSlot> Slots => Item.GetRequiredSlots();
-
         public int Quality => Item.GetComponent<ItemQualityComponent>()?.Quality ?? 0;
-
         public int Value => Item.GetComponent<ItemValueComponent>()?.Value ?? 0;
-
         public (int Cost, double Duration) Refining => (Functions.CalculateCraftingCost(Item, Owner), 
                                                         Functions.CalculateRefiningDuration(Item, Owner));
-
         public bool Crafted => Item.GetComponent<ItemRefinableComponent>()?.Refined ?? false;
 
+        public bool IsEquipped => Owner?.GetComponent<EquipmentComponent>()?.GetItems()?.Contains(Item) ?? false;
+        
+        
         private Entity Item { get; }
         private Entity? Owner { get; }
 
@@ -41,8 +38,52 @@ namespace TheIdleScrolls_Core.Items
             Owner = owner;
         }
 
+        private List<Entity> FindItemsToCompareTo()
+        {
+            if (IsEquipped)
+            {
+                return [];
+            }
+
+            bool IsComparable(Entity item)
+            {
+                if (item == Item)
+                {
+                    return false;
+                }
+                if (item.IsWeapon() != Item.IsWeapon()
+                    || item.IsArmor() != Item.IsArmor()
+                    || item.IsShield() != Item.IsShield())
+                {
+                    return false;
+                }
+                var itemSlots = item.GetRequiredSlots();
+                return itemSlots.All(Slots.Contains) || Slots.All(itemSlots.Contains);
+            }
+
+            var items = Owner?.GetComponent<EquipmentComponent>()?.GetItems() ?? [];
+            return items.Where(IsComparable)
+                        .ToList();
+        }
+
         private string GenerateDescription()
         {
+            var comparableItems = FindItemsToCompareTo();
+            string compToString(List<RelativeValue> results, bool higherIsBetter)
+            {
+                var relativeQualities = results.Select(r => r.ToRelativeQuality(higherIsBetter));
+                if (results.Count == 0)
+                {
+                    return "";
+                }
+                return " [" + String.Join("", relativeQualities.Select(cr => cr switch 
+                    { 
+                        RelativeQuality.Better => '+', 
+                        RelativeQuality.Worse => '-',
+                        _ => '='
+                    })) + "]";
+            }
+
             var itemComp = Item.GetComponent<ItemComponent>()!;
             string description = $"Type: {itemComp.Blueprint.GetFamilyDescription().Name}";
 
@@ -75,20 +116,21 @@ namespace TheIdleScrolls_Core.Items
             var weaponComp = Item.GetComponent<WeaponComponent>();
             if (weaponComp != null)
             {
-                description += $"; Damage: {weaponComp.Damage} [{(weaponComp.Damage / weaponComp.Cooldown):#.##} DPS]; ";
-                description += $"Attack Time: {weaponComp.Cooldown} s";
+                description += $"; Damage: {weaponComp.Damage}{compToString(ItemComparator.CompareDamage(Item, comparableItems), true)}";
+                description += $"; Attack Time: {weaponComp.Cooldown} s{compToString(ItemComparator.CompareCooldown(Item, comparableItems), false)}";
+                description += $"; DPS: {(weaponComp.Damage / weaponComp.Cooldown):#.##}{compToString(ItemComparator.CompareDps(Item, comparableItems), true)}";
             }
 
             var armorComp = Item.GetComponent<ArmorComponent>();
             if (armorComp != null)
             {
-                description += armorComp.Armor != 0.0 ? $"; Armor: {armorComp.Armor}" : "";
+                description += armorComp.Armor != 0.0 ? $"; Armor: {armorComp.Armor}{compToString(ItemComparator.CompareArmor(Item, comparableItems), true)}" : "";
                 description += armorComp.Evasion != 0.0 ? $"; Evasion: {armorComp.Evasion}" : "";
             }
 
             if (equipComp != null)
             {
-                description += equipComp.Encumbrance != 0.0 ? $"; Encumbrance: {equipComp.Encumbrance}%" : "";
+                description += equipComp.Encumbrance != 0.0 ? $"; Encumbrance: {equipComp.Encumbrance}%{compToString(ItemComparator.CompareEncumbrance(Item, comparableItems), false)}" : "";
             }
 
             var valueComp = Item.GetComponent<ItemValueComponent>();
@@ -99,25 +141,5 @@ namespace TheIdleScrolls_Core.Items
             return description;
         }
 
-        private string GetItemTypeName()
-        {
-            if (Item.IsWeapon())
-                return LocalizedStrings.Equip_Weapon;
-            if (Item.IsShield())
-                return LocalizedStrings.Equip_Shield;
-            if (Slots.Count == 0 || !Item.IsArmor()) // Should not happen with the current item kingdom
-                return "??";
-
-            if (Slots.Count > 1)
-                return "Custom Gear"; // Does not currently exist
-            return Slots[0] switch
-            {
-                EquipmentSlot.Head => LocalizedStrings.Equip_HeadArmor,
-                EquipmentSlot.Chest => LocalizedStrings.Equip_ChestArmor,
-                EquipmentSlot.Arms => LocalizedStrings.Equip_ArmArmor,
-                EquipmentSlot.Legs => LocalizedStrings.Equip_LegArmor,
-                _ => "??",
-            };
-        }
     }
 }
