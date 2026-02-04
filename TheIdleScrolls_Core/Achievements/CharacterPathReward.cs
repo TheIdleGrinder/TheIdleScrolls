06ede92fa74afb0a41a5f9ed7d344864dfcece8c
TheIdleGrinder <119699620+TheIdleGrinder@users.noreply.github.com>
2026-02-05 00:33:38 +0100

    Added reward type for character paths

---
+++ TheIdleScrolls_Core/Achievements/CharacterPathReward.cs
@@ -0,0 +1,26 @@
+﻿using MiniECS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheIdleScrolls_Core.CharacterPaths;
+using TheIdleScrolls_Core.Components;
+using TheIdleScrolls_Core.GameWorld;
+
+namespace TheIdleScrolls_Core.Achievements
+{
+    public class CharacterPathReward(CharacterPath Path) : IAchievementReward
+    {
+        public string Description => $"{Path.Name}";
+
+        public bool GiveReward(Entity entity, World world, Action<IMessage> postMessageCallback)
+        {
+            var pathComponent = entity.GetComponent<CharacterPathComponent>();
+            if (pathComponent is null)
+                return false;
+            pathComponent.AddPath(Path);
+            return true;
+        }
+    }
+}
