36d64158a6f178095dff3509d6b74910b30cc284
TheIdleGrinder <119699620+TheIdleGrinder@users.noreply.github.com>
2026-02-07 23:56:07 +0100

    Added fighter character path

--- TheIdleScrolls_Core/CharacterPaths/CharacterPathStep.cs
+++ TheIdleScrolls_Core/CharacterPaths/CharacterPathStep.cs
@@ -9,8 +9,7 @@ namespace TheIdleScrolls_Core.CharacterPaths
 {
     public class CharacterPathStep(string id,
                                    string name,
-                                   string description,
-                                   IAchievementReward reward)
+                                   string description)
     {
         public string Id { get { return id; } }
         public string Name { get { return name; } }
@@ -18,7 +17,7 @@ namespace TheIdleScrolls_Core.CharacterPaths
         public HashSet<string> StepTags { get; init; } = [];
         public CharacterPath? Path { get; set; }
         public Func<List<CharacterPathStep>, bool> SpecificAccess { get; init; } = (_) => true ;
-        public IAchievementReward Reward { get; init; } = reward;
+        public IAchievementReward Reward { get; init; }
 
         public bool CanBeTaken(List<CharacterPathStep> previousSteps)
         {
---
+++ TheIdleScrolls_Core/CharacterPaths/Paths/FighterPath.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheIdleScrolls_Core.Achievements;
+using TheIdleScrolls_Core.Definitions;
+using TheIdleScrolls_Core.Modifiers;
+
+namespace TheIdleScrolls_Core.CharacterPaths.Paths
+{
+    public static class FighterPath
+    {
+        const string PathId = "fighter";
+        const string RootId = "fighter_root";
+
+        public static CharacterPath Path { get; } = new CharacterPath(PathId, Properties.Skills.PathFighter, Properties.Skills.PathFighter);
+    
+        readonly static Perk RootPerk = new(RootId, Properties.Skills.PathFighterRoot, "", [], 
+            (l, e, w, c) => [
+                new($"{RootId}_dmg", ModifierType.Increase, 0.25, [Tags.Damage, Tags.Melee], []),
+                new($"{RootId}_arm", ModifierType.Increase, 0.25, [Tags.ArmorRating], [])
+            ])
+        {
+            Permanent = true
+        };
+
+        static FighterPath()
+        {
+            Path.AddStep(new CharacterPathStep(RootId, Properties.Skills.PathFighterRoot, "")
+            {
+                Reward = new PerkReward(RootPerk),
+                SpecificAccess = CharacterPathStep.MakeStandardRequirements("", 0)
+            });
+        }
+    }
+}
